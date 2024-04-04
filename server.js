80a008743ea29b3710acdd0ae24f69211b4ba935
Demarquies Jolley <ahmir1128@gmail.com>
2024-04-03 22:24:59 -0500

    lab o8 part 2

--- server.js
+++ server.js
@@ -24,6 +24,14 @@ class Forecast {
   }
 }
 
+class Movie {
+  constructor(movieObj) {
+    this.title = movieObj.title;
+    this.releaseDate = movieObj.release_date;
+    this.overview = movieObj.overview;
+  }
+}
+
   app.get('/weather', async (req, res) => {
     try {
       const {lat, lon} = req.query;
@@ -37,12 +45,32 @@ class Forecast {
     const forecasts = weatherData.map(day => new Forecast(day));
 
 
-  res.status(200).json(forecasts);
-    } catch (error) {
-      console.error('Error fetching weather data:', error);
-      res.status(500).json({ message: 'Internal Server Error'});
-    }  
+    res.status(200).json(forecasts);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+        res.status(500).json({ message: 'Internal Server Error'});
+      }  
   });
+
+  app.get('/movies', async (req, res) => {
+    // try {
+      const { city } = req.query;
+
+      const apiKeyMovie = process.env.MOVIE_API_KEY;
+      const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKeyMovie}&language=en-US&query=${city}&page=1&include_adult=false`);
+      
+      
+      const moviesData = response.data.results;
+      
+      const movies = moviesData.map(movie => new Movie(movie));
+      console.log("movie data", movies)
+
+      res.status(200).json(movies);
+    // } catch (error) {
+    //   console.error('Error fetching movie data:', error);
+    //   res.status(500).json({ message: 'Internal Server Error'});
+    // }
+  })
 
   app.get('/', (req, res) => {
     res.send('Hello, this is your Express server!');
