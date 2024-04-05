fc980fe4dc8c9456cf3fcd986c72217f98d3156c
Demarquies Jolley <ahmir1128@gmail.com>
2024-04-05 16:18:49 -0500

    lab 10

--- modules/movies.js
+++ modules/movies.js
@@ -12,9 +12,20 @@ class Movie {
     }
   };
 
+  //New
 async function getMovies(req, res) {
+  //New
     try {
+        const cache = {};
         const { city } = req.query;
+        //New
+        const cacheKey = `movies_${city}`;
+
+        //New
+        if (cache[cacheKey]) {
+          console.log('Movies data retrieved from cache');
+          return res.status(200).json(cache[cacheKey]);
+        }
   
         const apiKeyMovie = process.env.MOVIE_API_KEY;
         const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKeyMovie}&language=en-US&query=${city}&page=1&include_adult=false`);
@@ -24,6 +35,9 @@ async function getMovies(req, res) {
         
         const movies = moviesData.map(movie => new Movie(movie));
         console.log("movie data", movies)
+
+        //New
+        cache[cacheKey] = movies;
   
         res.status(200).json(movies);
       } catch (error) {
